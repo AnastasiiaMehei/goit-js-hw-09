a7d264c777b2eb1a1b1459408e25c433a61c8745
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-03-03 19:29:01 +0200

    1

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,19 @@
 const form = document.querySelector('.feedback-form');
 const email = form.elements.email;
 const message = form.elements.message;
-form.addEventListener('input', hundleInput);
-function handleInput(evt) {}
-const localStorageKey = {
-  email,
-  message,
-  key: feedback - form - state,
+
+const localStorageKey = 'feedback-form-state';
+const storedFormData = JSON.parse(localStorage.getItem(localStorageKey)) || {
+  email: '',
+  message: '',
 };
+email.value = storedFormData.email;
+message.value = storedFormData.message;
+form.addEventListener('input', handleInput);
+function handleInput() {
+  const object = { email: email.value.trim(), message: message.value.trim() };
+  setFormData(object);
+}
+function setFormData(object) {
+  localStorage.setItem('feedback-form-state', JSON.stringify(object));
+}
